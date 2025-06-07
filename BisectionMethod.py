4eecb34e5a35fd52e7d8701f189645b6acf47df1
Satyam Lamsal <lamsalsatyam@gmail.com>
2025-06-07 21:00:37 +0545

    Code correct up to noof iteration part

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -1,31 +1,29 @@
-import numpy as np
-
 class BisectionMethod:
-    def __init__(self, a, b, c,ran_a, ran_b):
+    def __init__(self, a, b, c, rn1, rn2,toleration):
         self.a = a
         self.b = b 
         self.c = c 
-        self.range_1 = ran_a
-        self.range_2 = ran_b
-        self.numberofiterations = 100
+        self.range_1 = rn1
+        self.range_2 = rn2
+        self.numberofiterations = 1000
         self.tempc = 0
         self.root = -1
+        self.tolerance = toleration
         
     def FunctionVal(self, x):
         return (self.a*(x**2) + self.b * x + self.c)
     
     def CalculateRootIteratively(self):
-        if((self.FunctionVal(self.range_1)) > 0 ):
+        if ((self.FunctionVal(self.range_1) * self.FunctionVal(self.range_2)) > 0):
             return False
 
-        while ( self.numberofiterations > 0):
-            self.tempc = (self.range_1 + self.range_2) / 2 
-            if((self.FunctionVal(self.tempc)>0) and (self.FunctionVal(self.range_1)<0)):
-                self.range_2 = self.tempc 
+        while (self.numberofiterations>0):            
+            self.tempc = (self.range_1 + self.range_2) / 2
+            if((self.tempc*self.a)>0):
+                self.a = self.tempc 
             else:
-                self.range_1 = self.tempc 
-
-            self.numberofiterations -= 1      
+                self.b = self.tempc    
+            self.numberofiterations -= 1                  
         self.root = self.tempc
         return True
     
@@ -34,6 +32,7 @@ class BisectionMethod:
 
 
 # y = x^2 -5x + 6
-BisecVar = BisectionMethod(1, -5, 6, 2, 4)
-BisecVar.CalculateRootIteratively()
-BisecVar.displayroot()
+
+BisecVar = BisectionMethod(1, -5, 6, 1.5, 2.5,0.0001)
+if BisecVar.CalculateRootIteratively():
+    BisecVar.displayroot()
