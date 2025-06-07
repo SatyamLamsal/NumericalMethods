278699bd39ae6e078819596c7ca9287a398aa8f2
Satyam Lamsal <lamsalsatyam@gmail.com>
2025-06-07 19:56:31 +0545

    changes in .py

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-
-
 class BisectionMethod:
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c,ran_a, ran_b):
         self.a = a
         self.b = b 
         self.c = c 
-        self.range_1 = 6
-        self.range_2 = 7
-        self.numberofiterations = 10
+        self.range_1 = ran_a
+        self.range_2 = ran_b
+        self.numberofiterations = 100
         self.tempc = 0
         self.root = -1
         
@@ -18,33 +16,24 @@ class BisectionMethod:
     
     def CalculateRootIteratively(self):
         if((self.FunctionVal(self.range_1)) > 0 ):
-            return False;
+            return False
 
         while ( self.numberofiterations > 0):
-            self.tempc = (self.range_1 + self.range_2) / 2;
+            self.tempc = (self.range_1 + self.range_2) / 2 
             if((self.FunctionVal(self.tempc)>0) and (self.FunctionVal(self.range_1)<0)):
-                self.range_2 = self.tempc;
+                self.range_2 = self.tempc 
             else:
-                self.range_1 = self.tempc;
+                self.range_1 = self.tempc 
 
-            self.numberofiterations -= 1
-        
+            self.numberofiterations -= 1      
         self.root = self.tempc
         return True
     
     def displayroot(self):
-        print(f"Positive Root of Polynomial is : {self.root:.4f}");
-                
-            
-a = 1
-b = -7
-c = 2
+        print(f"Positive Root of Polynomial is : {self.root:.4f}")
 
 
-print('Hello World!')
-
-
-
-SqFxn = BisectionMethod(a,b,c) 
-SqFxn.CalculateRootIteratively()
-SqFxn.displayroot()
+# y = x^2 -5x + 6
+BisecVar = BisectionMethod(1, -5, 6, 2, 4)
+BisecVar.CalculateRootIteratively()
+BisecVar.displayroot()
